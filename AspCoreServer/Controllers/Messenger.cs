75257329db58a8dd9b906c01f97d8c5ad38648aa
AndreyDe68 <68290233+AndreyDe68@users.noreply.github.com>
2021-09-22 14:17:50 +0300

    Добавлен сервер

---
+++ AspCoreServer/Controllers/Messenger.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspCoreServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Messenger : ControllerBase
+    {
+       
+
+        // GET api/<Messenger>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Hochu v vdv" + id.ToString();
+        }
+
+        // POST api/<Messenger>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+      
+    }
+}
