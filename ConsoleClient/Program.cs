8d58d37b1c243f80fac260723e7ac28c78ab250c
AndreyDe68 <68290233+AndreyDe68@users.noreply.github.com>
2021-09-22 14:05:06 +0300

    Добавлена сериализация

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,13 +1,20 @@
 ﻿using System;
-
+using Newtonsoft.Json;
 namespace MyMessenger
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("AndrewDe", "Hi!", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message desMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(desMsg);
+
+            //{ "Username":"AndrewDe","MessageText":"Hi!","TimeStamp":"2021-09-22T11:03:43.8272765Z"}
+            //AndrewDe < 22.09.2021 11:03:43 >: Hi!
+            //Console.WriteLine(msg.ToString());
         }
     }
 }
