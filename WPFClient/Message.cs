eb3165ab5d606dc58488ae39f9275ed8200eb90c
AndreyDe68 <68290233+AndreyDe68@users.noreply.github.com>
2021-09-26 14:23:31 +0300

    Добавлен WPFClient

---
+++ WPFClient/Message.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMessenger
+{
+    [Serializable]
+    public class Message
+    {
+        public string Username { get; set; }
+        public string MessageText { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public Message(string username, string messageText, DateTime timeStamp)
+        {
+            Username = username;
+            MessageText = messageText;
+            TimeStamp = timeStamp;
+        }
+        public Message()
+        {
+            Username = "System";
+            MessageText = "Server is running...";
+            TimeStamp = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            string output = String.Format("{0} <{2}>: {1}", Username, MessageText, TimeStamp);
+            return output;
+        }
+    }
+}
